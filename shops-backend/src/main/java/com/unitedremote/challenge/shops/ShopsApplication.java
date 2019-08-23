3a8410501a76b4b97a041d188a678b0bdb92293a
YOUSSEF CHLIH <usfchlih@gmail.com>
2019-08-24 00:41:55 +0100

    Initial project structure

---
+++ shops-backend/src/main/java/com/unitedremote/challenge/shops/ShopsApplication.java
@@ -0,0 +1,14 @@
+package com.unitedremote.challenge.shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ShopsApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopsApplication.class, args);
+	}
+
+}
